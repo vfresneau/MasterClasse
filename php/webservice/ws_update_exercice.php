98dc7eac3a4c1a36f280d15dd0f22133662728af
Chloe317 <94843849+Chloe317@users.noreply.github.com>
2022-03-30 15:22:02 +0200

    Update Exercice Reponse

--- php/webservice/ws_update_exercice.php
+++ php/webservice/ws_update_exercice.php
@@ -2,7 +2,7 @@
  require_once("../includes/config.php");
 
 
-$monExercice = new Exercice($_POST['id'],$_POST['nom'], $_POST['correction'], $_POST['consigne'], $_POST['reponseAttendu'], $_POST['valide'], $_POST['niveau'], $_POST['lien'], $_POST['id_theme']);
+$monExercice = new Exercice($_POST['id'],$_POST['nom'],"La réponse attendue était : ".$_POST['reponseAttendu'], $_POST['consigne'], $_POST['reponseAttendu'], $_POST['valide'], $_POST['niveau'], $_POST['lien'], $_POST['id_theme']);
 
 $monExercice->updateExercice();
 ?>
