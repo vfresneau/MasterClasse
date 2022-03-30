cb2fca2841cc8f9933d441d9498eb1700093a417
vfresneau <89484887+vfresneau@users.noreply.github.com>
2022-03-30 17:30:57 +0200

    commentaires 2

--- php/includes/config.php
+++ php/includes/config.php
@@ -2,11 +2,13 @@
 header("Access-Control-Allow-Origin: *");// 
 ini_set('display_errors', 1); // affiche toutes les erreurs
 error_reporting(E_ALL); // affiche toutes les erreurs
-
+//password and login required for each webservice
 const LOGIN="charley";
 const MDP="@JuNiRMdv5GZb";
-const SECRET = "Labrouette37!*qsdfreza89";
 
+//Password secret of jason web token (jwt)
+const SECRET = "Labrouette37!*qsdfreza89";
+//class required for pages webservices
 require_once("../classes/class_cours.php");
 require_once("../classes/class_examen.php");
 require_once("../classes/class_exercice_examen.php");
