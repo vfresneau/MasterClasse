6802988f3579bf2c08f1e41a194572599db500e0
vfresneau <89484887+vfresneau@users.noreply.github.com>
2022-03-31 09:40:11 +0200

    update reponse modif

--- php/webservice/ws_update_reponse.php
+++ php/webservice/ws_update_reponse.php
@@ -1,6 +1,11 @@
 <?php
- require_once("../includes/config.php");
-//je créer une instance de la classe reponse
+//I call on files includes
+require_once("../includes/config.php");
+//I create an instance of JWT class for use his fonctions
+$JWT= new JWT();
+ //I call an fonction of token verification to have access at this webservice
+$JWT-> testTokenExitValide();
+
     $maReponse =  new Reponse();
     //on hydrate notre insctance de la classe réponse avec la description et l'id de l'exercice reçu en paramettre
         $maReponse->set_DESCRIPTION($_POST["description"]);
